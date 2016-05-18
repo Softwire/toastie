63f3859dd4fc0c8a548cc4f825c05d7575804215
Jamie Humphries <jamie.humphries@softwire.com>
2016-05-18 10:40:09 +0100

    Add logo to sidebar.

--- src/scripts/Sidebar.tsx
+++ src/scripts/Sidebar.tsx
@@ -1,9 +1,12 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import * as React from "react";
+import { ToastLogo } from "./ToastLogo"
 
 export class Sidebar extends React.Component<{}, {}> {
   render() {
-    return <div id="sidebar"></div>
+    return <div id="sidebar">
+      <ToastLogo />
+    </div>;
   }
 }
---
+++ src/scripts/ToastLogo.tsx
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import * as React from "react";
+
+export class ToastLogo extends React.Component<{}, {}> {
+  render() {
+    return <img id="logo" src="content/logo.png" alt="Toast"/>;
+  }
+}
