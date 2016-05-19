5a998d2070cbfa8f4fe4b4970d169ba471290c9f
Jamie Humphries <jamie.humphries@softwire.com>
2016-05-19 14:16:30 +0100

    Change internal access to firebase in toast client.

--- src/scripts/ToastClient.ts
+++ src/scripts/ToastClient.ts
@@ -5,18 +5,20 @@ import { Toast } from "./models/Toast";
 
 export class ToastClient {
   private static FIREBASE_URL: string = "https://toastie-io.firebaseio.com/";
-  private static TOASTS_REF: Firebase = new Firebase(ToastClient.FIREBASE_URL).child("toasts");
+  private static firebase: Firebase = new Firebase(ToastClient.FIREBASE_URL);
+
+  private static get toastsRef() { return ToastClient.firebase.child("toasts"); }
 
   static sendToast(message: string) {
     var toast: Toast = {
       message: message,
       timestamp: Firebase.ServerValue.TIMESTAMP
     }
-    ToastClient.TOASTS_REF.push(toast);
+    ToastClient.toastsRef.push(toast);
   }
 
   static onToastAdded(callback: (toast: Toast) => any) {
-    ToastClient.TOASTS_REF.orderByChild("timestamp")
+    ToastClient.toastsRef.orderByChild("timestamp")
       .on("child_added", snapshot => {
         var toast = snapshot.val();
         callback(toast);
