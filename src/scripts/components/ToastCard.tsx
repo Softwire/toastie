5b51832972dc7fc4191c29bb27d06bb1bd44c821
Jamie Humphries <jamie.humphries@softwire.com>
2016-05-20 14:42:32 +0100

    Add header to toast card for sender and recipient.

--- src/scripts/components/ToastCard.tsx
+++ src/scripts/components/ToastCard.tsx
@@ -10,15 +10,24 @@ interface ToastCardProps {
 export class ToastCard extends React.Component<ToastCardProps, {}> {
   render() {
     return <div className="toast-card">
-      <div className="toast-image">
-        <span className="icon-toast"></span>
+      <div className="header">
+        <div>{ this.props.toast.from }</div>
+        <div className="toasted">
+          <span className="icon-toasted"></span>
+        </div>
+        <div>{ this.props.toast.to }</div>
       </div>
       <div className="toast-body">
-        <div className="message">
-          { this.props.toast.message }
+        <div className="toast-image">
+          <span className="icon-toast"></span>
         </div>
-        <div className="timestamp">
-          { new Date(this.props.toast.timestamp).toLocaleString() }
+        <div className="message-body">
+          <div className="message">
+            { this.props.toast.message }
+          </div>
+          <div className="timestamp">
+            { new Date(this.props.toast.timestamp).toLocaleString() }
+          </div>
         </div>
       </div>
     </div>;
