529cb27023a3f72f1390f83788beb0951e8b0a8e
Jamie Humphries <jamie.humphries@softwire.com>
2016-05-21 07:54:44 +0100

    Compile TypeScript to es5.

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,5 +1,6 @@
 /// <reference path="main.d.ts" />
 
+import * as Promise from "es6-promise";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ToastApp } from "./components/ToastApp";
